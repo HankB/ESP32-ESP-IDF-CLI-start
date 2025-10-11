7ca6fac65c1e34888755451d62e3ea899fffa2f1
Hank Barta <hbarta@gmail.com>
2025-10-10 19:21:38 -0500

    formatting, hard code NTP server

--- main/proj_sntp.c
+++ main/proj_sntp.c
@@ -1,13 +1,12 @@
 /* Provide SNTP support for an SSP-IDF based applicatio0n.
-* Heavily cribbed from examples/protocols/sntp/main/sntp_example_main.c
-*/
+ * Heavily cribbed from examples/protocols/sntp/main/sntp_example_main.c
+ */
 
 #include <time.h>
 #include <sys/time.h>
 #include <esp_system.h>
 #include <esp_sntp.h>
 #include <esp_log.h>
-
 
 #include "proj_sntp.h"
 
@@ -25,7 +24,6 @@ static const char *TAG = "sntp";
 static void obtain_time(void);
 static void initialize_sntp(void);
 
-
 void time_sync_notification_cb(struct timeval *tv)
 {
     ESP_LOGI(TAG, "Notification of a time synchronization event");
@@ -38,7 +36,8 @@ time_t init_sntp(void)
     time(&now);
     localtime_r(&now, &timeinfo);
     // Is time set? If not, tm_year will be (1970 - 1900).
-    if (timeinfo.tm_year < (2016 - 1900)) {
+    if (timeinfo.tm_year < (2016 - 1900))
+    {
         ESP_LOGI(TAG, "Time is not set yet. Connecting to WiFi and getting time over NTP.");
         obtain_time();
         // update 'now' variable with current time
@@ -49,15 +48,16 @@ time_t init_sntp(void)
 
 static void obtain_time(void)
 {
-    esp_sntp_servermode_dhcp(true);      // accept NTP offers from DHCP server, if any
+    sntp_servermode_dhcp(true); // accept NTP offers from DHCP server, if any
     initialize_sntp();
 
     // wait for time to be set
     time_t now = 0;
-    struct tm timeinfo = { 0 };
+    struct tm timeinfo = {0};
     int retry = 0;
     const int retry_count = 15;
-    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
+    while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count)
+    {
         ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
@@ -70,7 +70,7 @@ static void initialize_sntp(void)
     ESP_LOGI(TAG, "Initializing SNTP");
     esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
 
-    esp_sntp_setservername(0,  sntp_server);
+    esp_sntp_setservername(0, "2.us.pool.ntp.org");
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
     esp_sntp_init();
 }
