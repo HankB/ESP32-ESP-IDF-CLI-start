c376bbd5fa9807da3193fdd75087035d820fe99f
Hank Barta <hbarta@gmail.com>
2025-10-14 14:39:43 -0500

    static server - working

--- main/proj_sntp.c
+++ main/proj_sntp.c
@@ -1,5 +1,13 @@
 /* Provide SNTP support for an SSP-IDF based applicatio0n.
  * Heavily cribbed from examples/protocols/sntp/main/sntp_example_main.c
+ *
+ * Further documentation at
+ * https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/network/esp_netif_programming.html#esp-netif-sntp-api
+ *
+ * This example will configure SNTP to get the server from the DHCP assignment:https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/network/esp_netif_programming.html#use-dhcp-obtained-sntp-server-s
+ * Or it will not. The 'sntp' example code compiles fine with NTP server from DHCP but this file finds
+ * esp_netif_sntp_start() to not be included in the headers and I cannot figure out how to fix that so I'm switching
+ * to statically defined NTP server.
  */
 
 #include <time.h>
@@ -7,6 +15,7 @@
 #include <esp_system.h>
 #include <esp_sntp.h>
 #include <esp_log.h>
+#include "esp_netif_sntp.h"
 
 #include "proj_sntp.h"
 
@@ -35,6 +44,7 @@ time_t init_sntp(void)
     struct tm timeinfo;
     time(&now);
     localtime_r(&now, &timeinfo);
+    initialize_sntp();
     // Is time set? If not, tm_year will be (1970 - 1900).
     if (timeinfo.tm_year < (2016 - 1900))
     {
@@ -48,8 +58,10 @@ time_t init_sntp(void)
 
 static void obtain_time(void)
 {
+    /*
     sntp_servermode_dhcp(true); // accept NTP offers from DHCP server, if any
     initialize_sntp();
+    */
 
     // wait for time to be set
     time_t now = 0;
@@ -68,9 +80,22 @@ static void obtain_time(void)
 static void initialize_sntp(void)
 {
     ESP_LOGI(TAG, "Initializing SNTP");
-    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
 
-    esp_sntp_setservername(0, "2.us.pool.ntp.org");
+    /*
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG_MULTIPLE(0, {});
+    config.start = false;            // start the SNTP service explicitly
+    config.server_from_dhcp = false; // accept the NTP offer from the DHCP server
+    esp_netif_sntp_init(&config);
+    esp_netif_sntp_start();
+    */
+
+    esp_sntp_config_t config = ESP_NETIF_SNTP_DEFAULT_CONFIG_MULTIPLE(2,
+                                                                      ESP_SNTP_SERVER_LIST("time.windows.com", "pool.ntp.org"));
+    esp_netif_sntp_init(&config);
     sntp_set_time_sync_notification_cb(time_sync_notification_cb);
-    esp_sntp_init();
+    /*    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+
+        //esp_sntp_setservername(0, "2.us.pool.ntp.org");
+        esp_sntp_init();
+    */
 }
